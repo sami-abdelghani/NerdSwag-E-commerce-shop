eaa5fb7ad594839d62a6580522d4bb16123ff1cc
sami-abdelghani <62582814+sami-abdelghani@users.noreply.github.com>
2020-07-22 22:39:59 -0700

    Updated code for Front End E-commerce shop with Login, Registration, and logout features
    Adding the capability to login into an account, register an account, and logout of an account.

--- http-service.js
+++ http-service.js
@@ -3,6 +3,7 @@ import 'whatwg-fetch';
 /* A HttpService class to execute HTTP requests with
  the server connected to the MongoDB*/
 class HttpService{
+  /* Gets all the products from the server */
   getProducts = () => {
     let promise = new Promise((resolve, reject) => {
       fetch('http://localhost:3000/product').then(res => {
@@ -11,6 +12,17 @@ class HttpService{
   });
     return promise;
   }
+
+  /* Gets all the accounts from the server */
+  getAccounts = () => {
+    let promise = new Promise((resolve, reject) => {
+      fetch('http://localhost:3000/account').then(res => {
+        resolve(res.json());
+    })
+  });
+    return promise;
+  }
+
 }
 
 export default HttpService;
